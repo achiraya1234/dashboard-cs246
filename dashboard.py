92abe43779575d0d4f857641cb840c4054ef2e77
achiraya1234 <166907258+achiraya1234@users.noreply.github.com>
2024-04-15 03:53:36 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -153,8 +153,8 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
           stroke=alt.value('black'),
           strokeWidth=alt.value(0.25),
       ).properties(
-          width=350,
-          height=250,
+          width=400,
+          height=200,
       ).configure_axis(
           labelFontSize=12,
           titleFontSize=12
