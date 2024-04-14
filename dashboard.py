93968aa837c62d65768d8dbc9d2d13d8220da471
achiraya1234 <166907258+achiraya1234@users.noreply.github.com>
2024-04-15 03:34:56 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -114,7 +114,7 @@ color_scale = alt.Scale(
 
 legend_data = pd.DataFrame({'value': [5, 4, 3, 2, 1]})
 legend_bar = alt.Chart(legend_data).mark_rect().encode(
-    y=alt.Y('value:O', axis=alt.Axis(title='Value', labelAngle=0), sort=alt.SortOrder('descending')),
+    y=alt.Y('value:O', axis=alt.Axis(title='Value'), sort=alt.SortOrder('descending')),
     color=alt.Color('value:Q', scale=color_scale)
 )
 
